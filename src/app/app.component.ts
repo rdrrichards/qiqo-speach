dbf00e42276d044d11e9a3ac9ce0fd77bf962c0c
R. Richards <rdrrichards@hotmail.com>
2024-11-23 14:43:04 -0600

    cli update to latest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,10 @@ import { SpeachService } from './speach.service';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, FormsModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [CommonModule, FormsModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   public isUserSpeaking: boolean = false;
