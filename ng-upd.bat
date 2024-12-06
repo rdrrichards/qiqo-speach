aa4d2a26b0bcb79946ea4385c92f6e4057def492
R. Richards <rdrrichards@hotmail.com>
2024-12-06 08:48:39 -0600

    pre update commit

--- ng-upd.bat
+++ ng-upd.bat
@@ -5,8 +5,8 @@ CALL git commit -am "ng update to latest"
 CALL ng update @angular/cli --force
 CALL git add .
 CALL git commit -am "cli update to latest"
-CALL ng update @angular/cdk
-CALL git commit -am "cdk update to latest"
+REM CALL ng update @angular/cdk
+REM CALL git commit -am "cdk update to latest"
 CALL npm audit fix
 CALL git commit -am "post update commit"
 CALL ng b -c production
